9c3645daeb7569dc12bbd97d82a6cb80d8627493
Christopher Geary <dev@crgeary.com>
2019-11-12 17:44:36 +0000

    Bump version to 0.3.3

--- wp-jamstack-deployments.php
+++ wp-jamstack-deployments.php
@@ -6,7 +6,7 @@
  * Description: A WordPress plugin for JAMstack deployments on Netlify (and other platforms).
  * Author: Christopher Geary
  * Author URI: https://crgeary.com
- * Version: 0.3.2
+ * Version: 0.3.3
  */
 
 if (!defined('ABSPATH')) {
